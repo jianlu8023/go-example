fa489549d9d72d585f61e6a9fcdc2800cc807d0b
jianlu8023 <jianlu8023@gmail.com>
2024-08-19 18:49:40 +0800

    ipfs car

--- cmd/ipfs/car/car.go
+++ cmd/ipfs/car/car.go
@@ -5,13 +5,13 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"time"
+	"path/filepath"
 
 	"github.com/ipfs/boxo/gateway"
 	"github.com/ipfs/boxo/path"
 	"github.com/ipfs/go-cid"
+
 	"github.com/jianlu8023/go-example/pkg/logger"
-	fpath "github.com/jianlu8023/go-tools/pkg/path"
 )
 
 var appLogger = logger.GetAppLogger()
@@ -19,63 +19,69 @@ var appLogger = logger.GetAppLogger()
 func main() {
 
 	client := http.Client{
-		Timeout: 5 * time.Second,
+		// Timeout: 5 * time.Second,
 	}
 	backend, err := gateway.NewRemoteCarBackend([]string{"http://172.25.138.45:8080"}, &client)
 	if err != nil {
 		appLogger.Errorf("NewRemoteCarBackend Error: %v", err)
 		return
 	}
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancelFunc()
-	c, err := cid.Parse("bafybeigggyffcf6yfhx5irtwzx3cgnk6n3dwylkvcpckzhqqrigsxowjwe")
-	if err != nil {
-		appLogger.Errorf("parse cid Error %v", err)
-		return
+	ctx := context.Background()
+	m := map[string]string{
+		"bafybeigggyffcf6yfhx5irtwzx3cgnk6n3dwylkvcpckzhqqrigsxowjwe": "0.28.0webui.car",
+		"bafybeif4zkmu7qdhkpf3pnhwxipylqleof7rl6ojbe7mq3fzogz6m4xk3i": "0.8.0webui.car",
 	}
+	for k, v := range m {
+		c, err := cid.Parse(k)
+		if err != nil {
+			appLogger.Errorf("parse cid Error %v", err)
+			return
+		}
+		immutablePath, err := path.NewImmutablePath(path.FromCid(c))
+		if err != nil {
+			appLogger.Errorf("p.NewImmutablePath Error: %v", err)
+			return
+		}
+		car, closer, err := backend.GetCAR(ctx, immutablePath, gateway.CarParams{
+			Scope: gateway.DagScopeAll,
+		})
+		if err != nil {
+			appLogger.Errorf("GetCAR Error: %v", err)
+			return
+		}
+		appLogger.Infof("car: %v, closer: %v", car, closer)
+		// defer func(closer io.ReadCloser) {
+		// 	err := closer.Close()
+		// 	if err != nil {
+		// 		appLogger.Errorf("closer.Close Error: %v", err)
+		// 	}
+		// }(closer)
 
-	immutablePath, err := path.NewImmutablePath(path.FromCid(c))
-	if err != nil {
-		appLogger.Errorf("p.NewImmutablePath Error: %v", err)
-		return
-	}
+		p := "./testdata/" + v
+		dir := filepath.Dir(p)
+		err = os.MkdirAll(dir, os.ModeDir)
+		if err != nil {
+			appLogger.Errorf("os.MkdirAll Error: %v", err)
+			return
+		}
 
-	car, closer, err := backend.GetCAR(ctx, immutablePath, gateway.CarParams{
-		Scope: gateway.DagScopeAll,
-	})
-
-	if err != nil {
-		appLogger.Errorf("GetCAR Error: %v", err)
-		return
-	}
-	appLogger.Infof("car: %v, closer: %v", car, closer)
-	defer func(closer io.ReadCloser) {
-		err := closer.Close()
+		create, err := os.Create(p)
+		if err != nil {
+			appLogger.Errorf("os.Create Error: %v", err)
+			return
+		}
+		_, err = io.Copy(create, closer)
+		if err != nil {
+			appLogger.Errorf("io.Copy Error: %v", err)
+			return
+		}
+		err = closer.Close()
 		if err != nil {
 			appLogger.Errorf("closer.Close Error: %v", err)
-
 		}
-	}(closer)
-	p := "./testdata/test.car"
 
-	err = fpath.Ensure(p[:len(p)-8])
-
-	if err != nil {
-
-		appLogger.Errorf("path.Ensure Error: %v", err)
-		return
-
+		appLogger.Infof("get %v car success.", v)
 	}
 
-	create, err := os.Create(p)
-	if err != nil {
-		appLogger.Errorf("os.Create Error: %v", err)
-		return
-	}
-
-	_, err = io.Copy(create, closer)
-	if err != nil {
-		appLogger.Errorf("io.Copy Error: %v", err)
-		return
-	}
+	appLogger.Infof("all car get it.")
 }
