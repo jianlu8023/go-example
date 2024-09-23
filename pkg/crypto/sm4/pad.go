3146757ea5f0154744f60cad2e09edd7141600b1
jianlu8023 <jianlu8023@gmail.com>
2024-09-23 19:58:41 +0800

    crypto

---
+++ pkg/crypto/sm4/pad.go
@@ -0,0 +1,30 @@
+package sm4
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/tjfoc/gmsm/sm4"
+)
+
+const blockSize = sm4.BlockSize // SM4 块大小为 16 字节
+
+// PKCS7Padding 添加 PKCS#7 填充
+func PKCS7Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+// PKCS7UnPadding 移除 PKCS#7 填充
+func PKCS7UnPadding(src []byte) ([]byte, error) {
+	length := len(src)
+	if length == 0 {
+		return nil, fmt.Errorf("输入为空")
+	}
+	unpadding := int(src[length-1])
+	if unpadding > length || unpadding > blockSize {
+		return nil, fmt.Errorf("无效的填充")
+	}
+	return src[:length-unpadding], nil
+}
