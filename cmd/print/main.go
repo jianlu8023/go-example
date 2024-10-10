00d23f0ae96d11e4576017526f0fbdb10cfbaded
jianlu8023 <jianlu8023@gmail.com>
2024-10-10 11:15:08 +0800

    string方法

--- cmd/print/main.go
+++ cmd/print/main.go
@@ -19,6 +19,19 @@ func (u User) String() string {
 	return marshalString
 }
 
+// Animal 结构体
+type Animal struct {
+	Name string `json:"name"`
+	Age  int64  `json:"age"`
+}
+
+// String string方法
+// @return string: json格式的string
+func (a *Animal) String() string {
+	marshalString, _ := sonic.MarshalString(a)
+	return marshalString
+}
+
 var (
 	log = logger.GetAppLogger()
 )
@@ -29,4 +42,16 @@ func main() {
 	log.Infof("%v", u1)
 	u2 := &User{Name: "u2 *User", Age: 18}
 	log.Infof("%v", u2)
+
+	a1 := Animal{
+		Name: "a1 Animal",
+		Age:  18,
+	}
+	log.Infof("%v", a1)
+	a2 := &Animal{
+		Name: "a2 *Animal",
+		Age:  18,
+	}
+	log.Infof("%v", a2)
+
 }
