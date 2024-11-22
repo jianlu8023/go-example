98c238c53aae7868797215e628749e614b3c11c9
jianlu8023 <jianlu8023@gmail.com>
2024-11-22 17:06:29 +0800

    gin的tls

---
+++ cmd/web/gin/tls/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jianlu8023/go-example/internal/logger"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.Any("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		if err := srv.ListenAndServeTLS("./cert/rsa.crt", "./cert/rsa.key"); err != nil {
+			logger.GetAppLogger().Errorf("启动gin web服务失败 %v", err)
+			quit <- syscall.SIGINT
+		}
+	}()
+
+	<-quit
+	if err := srv.Shutdown(context.Background()); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return
+		}
+		logger.GetAppLogger().Errorf("关闭gin web服务失败 %v", err)
+	}
+
+}
